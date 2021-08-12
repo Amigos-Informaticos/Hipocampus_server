150d69144bd1f685f3731ae4f808a47be231109c
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2021-08-11 22:59:15 -0500

    Se han actualizado la credencial SSL

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask_cors import CORS
 
 from src.routes.events import events_routes
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, support_credentials=True)
 
 app.register_blueprint(events_routes)
 
@@ -14,5 +14,16 @@ def hello_world():
 	return 'Hello World!'
 
 
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>", methods=["OPTIONS"])
+def prefligth(path):
+	response = make_response()
+	response.headers.add("Access-Control-Allow-Origin", "*")
+	response.headers.add('Access-Control-Allow-Headers', "*")
+	response.headers.add('Access-Control-Allow-Methods', "*")
+	return response
+
+
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=42080, ssl_context=("cert.pem", "key.pem"))
+# app.run(host="0.0.0.0", port=42080)
--- src/routes/events.py
+++ src/routes/events.py
@@ -1,10 +1,11 @@
 import json
 
-from flask import Blueprint, Response
+from flask import Blueprint, Response, request
 from pymongo import MongoClient
 
 from src.configuration.JSONhandler import JSONHandler
 from src.configuration.paths import api_configuration_path
+from src.routes.HTTPStatus import BAD_REQUEST, NOT_FOUND, NO_CONTENT, OK
 
 connection = MongoClient(JSONHandler.load("connection_string", False, api_configuration_path))
 collection = connection["hipocampus"]["events"]
@@ -14,11 +15,37 @@ events_routes = Blueprint("events_routes", __name__)
 
 @events_routes.route("/events", methods=["GET"])
 def get_events():
-	response = Response(status=204)
 	fechas: list = collection.find_one({})["fechas"]
-	response = Response(
-		json.dumps(fechas),
-		status=200,
-		mimetype="application/json"
-	)
+	response = Response(status=NO_CONTENT)
+	if fechas:
+		response = Response(
+			json.dumps(fechas),
+			status=OK,
+			mimetype="application/json"
+		)
+	return response
+
+
+@events_routes.route("/events", methods=["POST"])
+def save_event():
+	json_values = request.json
+	response = Response(status=BAD_REQUEST)
+	if "fecha" in json_values:
+		fechas: list = collection.find_one({})["fechas"]
+		fechas.append(json_values["fecha"])
+		collection.update_one({"name": "edson"}, {"$set": {"fechas": fechas}})
+		response = Response(status=OK)
+	return response
+
+
+@events_routes.route("/events", methods=["DELETE"])
+def delete_event():
+	json_values = request.json
+	response = Response(status=BAD_REQUEST)
+	if "fecha" in json_values:
+		fechas: list = collection.find_one({})["fechas"]
+		response = Response(status=NOT_FOUND)
+		if json_values["fecha"] in fechas:
+			fechas.remove(json_values["fecha"])
+			response = Response(status=OK)
 	return response
